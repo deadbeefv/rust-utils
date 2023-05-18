8bde94b4f3a01c1de27f1e720934faabe1ee8d7d
Machar <jkook2012@gmail.com>
2023-05-18 11:35:36 +0300

    uds connects, but there are some bugs to be fixed

--- src/proxy/uds.rs
+++ src/proxy/uds.rs
@@ -3,44 +3,41 @@ use rand::distributions::{Alphanumeric, DistString};
 use std::io::Error;
 use std::pin::Pin;
 use std::task::{Context, Poll};
-
-#[cfg(target_os = "windows")]
-use tokio::io::{AsyncRead, AsyncWrite};
+use std::env::temp_dir;
 
 #[cfg(not(target_os = "windows"))]
 use tokio::net::{UnixListener, UnixStream};
 
 #[cfg(target_os = "windows")]
-use uds_windows::{UnixListener, UnixStream};
+use uds_windows::{UnixStream, UnixListener};
 
-#[cfg(not(target_os = "windows"))]
+// #[cfg(target_os = "windows")]
+// use tokio_uds_windows::UnixStream;
+// #[cfg(target_os = "windows")]
+// use tokio_uds_windows::UnixListener;
+
 pub struct UDSConnector {
     inner: UnixListener,
     path: String,
 }
 
-#[cfg(target_os = "windows")]
-pub struct UDSConnector {
-    inner: UnixListener,
-    path: String,
-}
 
 impl UDSConnector {
+
+    pub fn new(path: String) -> Result<Self, Error> {
+        let uds = UnixListener::bind(&path)?;
+        Ok(UDSConnector { inner: uds, path })
+    }
     #[cfg(not(target_os = "windows"))]
-    pub fn new(path: String) -> Result<Self, Error> {
-        let uds = UnixListener::bind(&path)?;
-        Ok(UDSConnector { inner: uds, path })
-    }
-
-    #[cfg(target_os = "windows")]
-    pub fn new(path: String) -> Result<Self, Error> {
-        let uds = UnixListener::bind(&path)?;
-        Ok(UDSConnector { inner: uds, path })
-    }
-
     pub fn new_random() -> Result<Self, Error> {
         let mut rname = Alphanumeric.sample_string(&mut rand::thread_rng(), 8);
         rname = format!("/tmp/proxy-{}.sock", rname);
+        Self::new(rname)
+    }
+    #[cfg(target_os = "windows")]
+    pub fn new_random() -> Result<Self, Error> {
+        let mut rname = Alphanumeric.sample_string(&mut rand::thread_rng(), 8);
+        rname = format!("{}proxy-{}.sock", temp_dir().display(),rname);
         Self::new(rname)
     }
     pub fn get_path(&self) -> &str {
@@ -49,35 +46,31 @@ impl UDSConnector {
 }
 
 impl Accept for UDSConnector {
-    #[cfg(not(target_os = "windows"))]
     type Conn = UnixStream;
-
-    #[cfg(target_os = "windows")]
-    type Conn = UnixStream;
-    
     type Error = Error;
+
     #[cfg(not(target_os = "windows"))]
     fn poll_accept(
         self: Pin<&mut Self>,
         cx: &mut Context<'_>,
     ) -> Poll<Option<Result<Self::Conn, Self::Error>>> {
-        match self.inner.poll_accept(cx) {
+        match self.inner.poll_accept {
             Poll::Pending => Poll::Pending,
             Poll::Ready(Ok((socket, _addr))) => Poll::Ready(Some(Ok(socket))),
             Poll::Ready(Err(err)) => Poll::Ready(Some(Err(err))),
         }
     }
-
     #[cfg(target_os = "windows")]
     fn poll_accept(
         self: Pin<&mut Self>,
         cx: &mut Context<'_>,
     ) -> Poll<Option<Result<Self::Conn, Self::Error>>> {
-        match self.inner.accept() {
-            Ok(T) => Poll::Ready(Some(Ok(T.0))),
-            Err(E) => Poll::Ready(Some(Err(E)))
+        match self.inner.poll_accept() {
+            Ok(socket) => Poll::Ready(Some(Ok(socket))),
+            Err(e) => Poll::Ready(Some(Err(e)))
         }
     }
+
 }
 
 impl Drop for UDSConnector {
